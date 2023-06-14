b0bf6a3eb5052056b2ec5c37bbbc6d471771db0d
Bao Tran <tqbbao@gmail.com>
2023-06-14 09:49:27 +0700

    Git SlangWord Project

---
+++ src/pkg22424001/Main.java
@@ -0,0 +1,16 @@
+package pkg22424001;
+
+/**
+ *
+ * @author MSI VN
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
